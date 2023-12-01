f531760e348bcc3475d2d84a7f1bcb0ab27dcfec
EUcunguyabe <e.ucunguyab@alustudent.com>
2023-12-01 19:12:04 +0000

    task 0

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request and displays the size of the body of the response
-<<<<<<< HEAD
 curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
-=======
 curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
->>>>>>> 7fd0ad84a6e8e0c6d0eff4b4a67324685e5e8a94
