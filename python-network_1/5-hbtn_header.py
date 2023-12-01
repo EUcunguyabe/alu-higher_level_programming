9f65744ce2412c9dd471ab482a91138920587969
EUcunguyabe <e.ucunguyab@alustudent.com>
2023-12-01 20:21:31 +0000

    jdjd

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """__summary__
-- Write a Python script that
-- fetches https://intranet.hbtn.io/status.
+- Write a Python script that takes in a URL,
+- sends a request to the URL and displays the value of the
+- variable X-Request-Id in the response header
 """
+import sys
 import requests
 
-
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+if __name__ == '__main__':
+    reqs = requests.get(sys.argv[1])
+    print(reqs.headers.get("X-Request-Id"))
